abc49f39f3298f2d1c00719ddf4a7dc253f480fc
jonatan <jonatan.kruszewski@itc.tech>
2021-10-20 12:39:43 +0300

    Added new exercise

---
+++ Exercises/5.js
@@ -0,0 +1,15 @@
+/**
+ * Create a function that receives two parameters.
+ * The first one should be a placeholder for a memoized value.
+ * The second one would be a callback funciton.
+ * On the first call of curry, you should return a function that returns the callback function
+ * on the second call, you should run the returned function.
+ * });
+ * @param {number} number
+ * @param {Function} callback
+ */
+function curry(number, callback) {
+  //
+}
+
+export default curry;
---
+++ tests/5.test.js
@@ -0,0 +1,18 @@
+import curry from "../Exercises/5";
+describe("Testing Exercise 5", () => {
+  let fc;
+  beforeAll(() => {
+    fc = a => a + 5;
+  });
+
+  test("Currying", () => {
+    const curried = curry(5, callback);
+    const secondCall = curried();
+    expect(secondCall).toStrictEqual(10);
+  });
+  test("Currying 2", () => {
+    const curried = curry(15, callback);
+    const secondCall = curried();
+    expect(secondCall).toStrictEqual(20);
+  });
+});
